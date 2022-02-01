0878fa9a63b59684269d2320ac4adec048e3f5d1
Saravanan Suresh <98247476+SaravananSuresh7@users.noreply.github.com>
2022-02-01 18:42:40 +0530

    Add files via upload

---
+++ Testcase1.java
@@ -0,0 +1,23 @@
+package week2.assignments2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Testcase1 {
+	public static void main(String[] args) {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://acme-test.uipath.com/login");
+		driver.manage().window().maximize();
+		driver.findElement(By.id("email")).sendKeys("kumar.testleaf@gmail.com");
+		driver.findElement(By.id("password")).sendKeys("leaf@12");
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		String title = driver.getTitle();
+		System.out.println(title);
+		driver.findElement(By.xpath("//a[@href='https://acme-test.uipath.com/logout']")).click();
+		driver.close();
+
+	}
+
+}
