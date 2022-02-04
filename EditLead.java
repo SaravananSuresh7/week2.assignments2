54b87a21bfe09228ec101519bcde7ce9cb7a4d55
Saravanan Suresh <98247476+SaravananSuresh7@users.noreply.github.com>
2022-02-04 14:47:25 +0530

    Add files via upload

---
+++ EditLead.java
@@ -0,0 +1,34 @@
+package week2.assignments2;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class EditLead {
+	public static void main(String[] args) throws InterruptedException {
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().window().maximize();
+		WebElement elementUsername = driver.findElement(By.id("username"));
+		elementUsername.sendKeys("Democsr");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.partialLinkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Find Leads")).click();
+		driver.findElement(By.xpath("(//input[@name='firstName'])[3]")).sendKeys("Babu");
+		driver.findElement(By.xpath("//button[text()='Find Leads']")).click();
+		driver.findElement(By.xpath("(//a[@href = '/crmsfa/control/viewLead?partyId=10036'])[1]")).click();
+		System.out.println(driver.getTitle());
+		driver.findElement(By.xpath("//a[text() ='Edit']")).click();
+		driver.findElement(By.id("updateLeadForm_companyName")).clear();
+		driver.findElement(By.id("updateLeadForm_companyName")).sendKeys("TCS");
+		driver.findElement(By.xpath("//input[@value = 'Update']")).click();
+		driver.close();
+
+	}
+
+}
